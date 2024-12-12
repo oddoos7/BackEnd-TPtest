27b079e6db8df996040530ec3bc8f10644a50028
vip03 <vip03@LAPTOP-LLETA8TF>
2024-12-12 13:31:55 +0900

    🎉 Term Project Backend 시작

---
+++ src/test/kotlin/kr/ac/kumoh/s20200081/BackEnd_TPtest/BackEndTPtestApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20200081.BackEnd_TPtest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackEndTPtestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
